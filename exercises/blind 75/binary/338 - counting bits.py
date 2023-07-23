e4455a43c7984f589184d40dd9d2715f7d46778a
vsaraikin <vsaraikin@splunk.com>
2023-07-23 20:38:11 +0200

    add new books, solved some problems

---
+++ exercises/blind 75/binary/338 - counting bits.py
@@ -0,0 +1,11 @@
+import math
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        z = 0
+        for i in range(1,n):
+            math.comb(1,)
+        return z
+    
+s = Solution()
+print(s.climbStairs(2))
---
+++ structures/heap/heap.py
@@ -0,0 +1,21 @@
+class Heap:
+    def __init__(self) -> None:
+        pass
+    
+    def push():
+        pass
+    
+    def pop():
+        pass
+    
+    def find():
+        pass
+    
+    def heapify():
+        pass
+    
+    def sort():
+        pass
+    
+
+l = [1, 2, 3, 4, 5]
