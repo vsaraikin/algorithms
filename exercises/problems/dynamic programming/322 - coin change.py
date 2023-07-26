8f30951e4016f47acc5d50b5142810b0e302c2bb
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2023-07-26 18:03:32 +0200

    solved some problems

--- exercises/problems/dynamic programming/322 - coin change.py
+++ exercises/problems/dynamic programming/322 - coin change.py
@@ -1,33 +1,12 @@
 coins1 = [1,2,5]
 coins2 = [2]
 coins3 = [1]
-coins4 = [186,419,83,408]
-
 
 amount1 = 11
 amount2 = 3
 amount3 = 0
-amount4 = 6249
-
-
 
 class Solution:
-    # def coinChange(self, coins: list[int], amount: int) -> int:
-        # pairs = []
-        
-        # while True:
-        #     if not coins:
-        #         return -1
-            
-        #     s = sum(pairs)
-        #     if s < amount:
-        #         pairs.append(max(coins))
-        #     elif s == amount:
-        #         return len(pairs)
-        #     else:
-        #         coins.remove(max(coins))
-        #         pairs.pop()
-    
     def coinChange(self, coins: list[int], amount: int) -> int:
         arr = [amount + 1] * (amount + 1)
         arr[0] = 0
@@ -40,14 +19,10 @@ class Solution:
         if arr[amount] > amount:
             return -1 
         else:
-            return arr[amount] 
-        
-
-    
+            return arr[amount]    
         
 s = Solution()
-print(s.coinChange(coins1, amount1))
-print(s.coinChange(coins2, amount2))
-print(s.coinChange(coins3, amount3))
-print(s.coinChange(coins4, amount4))
+assert 3 == s.coinChange(coins1, amount1)
+assert -1 ==  s.coinChange(coins2, amount2)
+assert 0 == s.coinChange(coins3, amount3)
 
