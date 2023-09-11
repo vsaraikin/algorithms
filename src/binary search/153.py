16f7ebdf9cbfd3ec80317f1bd190a0c3aa0cd7bc
Vladimir Saraikin <saraikin.v@phystech.edu>
2023-09-11 19:35:53 +0200

    moved 153 and solved 33

--- src/binary search/153.py
+++ src/binary search/153.py
@@ -1,7 +1,3 @@
-nums = [3,4,5,1,2]
-nums2 = [4,5,6,7,0,1,2]
-nums3 = [11,13,15,17]
-
 class Solution:
     def findMin(self, elements: list[int]) -> int:
         if len(elements) == 2:
@@ -23,6 +19,6 @@ class Solution:
     
         
 s = Solution()
-print(s.findMin(nums))
-print(s.findMin(nums2))
-print(s.findMin(nums3))
+assert s.findMin([3,4,5,1,2]) == 1
+assert s.findMin([4,5,6,7,0,1,2]) == 0
+assert s.findMin([11,13,15,17]) == 11
