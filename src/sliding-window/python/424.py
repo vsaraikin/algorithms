427088e071ac23d348b4b2eba0701fac04112d04
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-02-02 13:32:06 +0100

    solved 424

--- src/sliding-window/python/424.py
+++ src/sliding-window/python/424.py
@@ -1,24 +1,18 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        freq_storage = {}
+        left, right = 0, 0
+        max_count = 0 # highest frequency
         max_length = 0
-        max_freq = 0 
-        left = 0
-        char_count = {}
         
-        for right in range(len(s)):
-            
-            char_count[s[right]] = char_count.get(s[right], 0) + 1
-            
-            max_freq = max(char_count[s[right]], max_freq)
-            
-            changes_needed = (right - left + 1) - max_freq
-            
-            if changes_needed > k:
-                char_count[s[left]] -= 1
+        for right, char in enumerate(s):
+            freq_storage[char] = freq_storage.get(char, 0) + 1
+            max_count = max(max_count, freq_storage[char])
+            while (right - left + 1) - max_count > k:
+                freq_storage[s[left]] -= 1
                 left += 1
             
             max_length = max(max_length, right - left + 1)
-
         
         return max_length
        
