99c99961772f2b05955d98e22a9f224754776bd9
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-02-02 09:46:28 +0100

    re-solved 121

--- src/sliding-window/python/121.py
+++ src/sliding-window/python/121.py
@@ -1,15 +1,17 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         profit = 0
-        last_min = prices[0]
+        buy_price = prices[0]
         for i in range(1, len(prices)):
-            if prices[i-1] < last_min:
-                last_min = prices[i-1]
-                
-            if prices[i] - last_min > profit:
-                profit = prices[i] - last_min
-                
-        return int(profit)
+            if prices[i] < buy_price:
+                buy_price = prices[i]
+            else:
+                if profit < prices[i] - buy_price:
+                    profit = prices[i] - buy_price
+
+        
+        return profit
+        
     
 s = Solution()
 assert 5 == s.maxProfit([7,1,5,3,6,4])
