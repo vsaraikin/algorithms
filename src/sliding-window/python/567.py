6fab16a7c57c8763cf9895cb53521addbc0fc81f
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-02-04 09:56:57 +0100

    solved 567

--- src/sliding-window/python/567.py
+++ src/sliding-window/python/567.py
@@ -1,27 +1,26 @@
+from collections import defaultdict
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
         
-        s1_freq = {}
-        s2_freq = {}
-        
-        for char in s1:
-            s1_freq[char] = s1_freq.get(char, 0) + 1
+        s1_freq = defaultdict(int)
+        s2_freq = defaultdict(int)
         
         for i in range(len(s1)):
-            s2_freq[s2[i]] = s2_freq.get(s2[i], 0) + 1
+            s1_freq[s1[i]] += 1
+            s2_freq[s2[i]] += 1
         
         if s1_freq == s2_freq:
             return True
         
         for i in range(len(s1), len(s2)):
-            if s2_freq[s2[i - len(s1)]] == 1:
-                del s2_freq[s2[i - len(s1)]]
-            else:
-                s2_freq[s2[i - len(s1)]] -= 1
+            s2_freq[s2[i]] += 1
+            s2_freq[s2[i - len(s1)]] -= 1
             
-            s2_freq[s2[i]] = s2_freq.get(s2[i], 0) + 1
+            if s2_freq[s2[i - len(s1)]] == 0:
+                del s2_freq[s2[i - len(s1)]]
             
             if s1_freq == s2_freq:
                 return True
