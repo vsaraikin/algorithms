51b45cb2e18898c2850798f1b3c3fed2e1062941
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-02-04 10:03:05 +0100

    solution with ord

--- src/sliding-window/python/567.py
+++ src/sliding-window/python/567.py
@@ -27,6 +27,31 @@ class Solution:
         
         return False
 
+    # solution with ord instead    
+    def checkInclusionOrd(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1_freq = [0] * 26
+        s2_freq = [0] * 26
+        a_ord = ord('a')
+        
+        for i in range(len(s1)):
+            s1_freq[ord(s1[i]) - a_ord] += 1
+            s2_freq[ord(s2[i]) - a_ord] += 1
+        
+        if s1_freq == s2_freq:
+            return True
+        
+        for i in range(len(s1), len(s2)):
+            s2_freq[ord(s2[i]) - a_ord] += 1
+            s2_freq[ord(s2[i - len(s1)]) - a_ord] -= 1
+            
+            if s1_freq == s2_freq:
+                return True
+        
+        return False
+
 
 s = Solution()
 assert s.checkInclusion("ab", "eidboaoo") == False
