ba34cf86a7138ee61147f0dec275ff2a9648bf00
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-02-09 09:48:27 +0100

    resolved 739

--- src/stack/python/739.py
+++ src/stack/python/739.py
@@ -1,17 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
         stack = []
-
         n = len(temperatures)
         results = [0] * n
-
         for i in range(n):
-            while stack and temperatures[i] > stack[-1][0]:
-                popped = stack.pop()[1]
-                results[popped] = i - popped
-            stack.append((temperatures[i], i))
-            
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                results[prev_index] = i - prev_index
+                
+            stack.append(i)
         return results
+                
                     
                     
         
