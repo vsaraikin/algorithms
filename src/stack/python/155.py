9ecfa1318d74f52724d10c20168034862f01fb41
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-02-09 08:50:56 +0100

    resolved 155

--- src/stack/python/155.py
+++ src/stack/python/155.py
@@ -2,29 +2,24 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.min_values = []
+        self.min_stack = []
+        
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-
-        if len(self.min_values) >= 1:
-            if self.min_values[-1] >= self.stack[-1]:
-                self.min_values.append(val)
-        else:
-            self.min_values.append(val)
-  
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
 
     def pop(self) -> None:
-        if self.stack[-1] == self.min_values[-1]:
-            self.min_values.pop()
-            
-        return self.stack.pop()
+        val = self.stack.pop()
+        if val == self.min_stack[-1]:
+            self.min_stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.min_values[-1]
+        return self.min_stack[-1]
     
     
 obj = MinStack()
