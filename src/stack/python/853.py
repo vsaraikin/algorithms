e898c1171bbcd952c1fabeef365a634b87e2719f
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-02-10 08:57:31 +0100

    solved 853

--- src/stack/python/853.py
+++ src/stack/python/853.py
@@ -1,17 +1,24 @@
 class Solution:
+    # neetcode solution
     def carFleet(self, target: int, position: list[int], speed: list[int]) -> int: 
         stack = []
         for pos, sp in sorted(zip(position, speed),reverse = True):
-
-            dist = target - pos
-            time = dist / sp 
+            stack.append((target - pos) / sp)
+            if len(stack) == 2 and stack[-1] <= stack[-2]:
+                stack.pop()
             
-            if not stack:
-                stack.append(time)
-            elif time > stack[-1]:
-                stack.append(time)
-
         return len(stack)
+    
+    # gpt solution
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int: 
+        cars = [(pos, (target - pos) / spd) for pos, spd in zip(position, speed)]
+        cars.sort(reverse=True)
+        current_time, fleets = 0, 0
+        for pos, time in cars:
+            if time > current_time:
+                current_time = time
+                fleets += 1
+        return fleets
 
 s = Solution()
 assert s.carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3])
