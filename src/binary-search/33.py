0ef69ce93595b8250cdeaa6e30c4344e1d852dce
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-02-15 11:35:05 +0100

    solved 33 and 153

--- src/binary-search/33.py
+++ src/binary-search/33.py
@@ -1,22 +1,18 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        left = 0 
-        right = len(nums) - 1
+        left, right = 0, len(nums) - 1
         while left <= right:
-            mid = left + (right - left) // 2
-            
+            mid = (right + left) // 2
             if nums[mid] == target:
                 return mid
             
-            if nums[mid] >= nums[left]:
-                # Left side is sorted.
-                if nums[left] <= target < nums[mid]:
+            if nums[left] <= nums[mid]: # left is sorted
+                if nums[left] <= target < nums[mid]: # target is in the left half
                     right = mid - 1
                 else:
                     left = mid + 1
-            else:
-                # Right side is sorted.
-                if nums[mid] < target <= nums[right]:
+            else: # right is sorted
+                if nums[mid] < target <= nums[right]: # target is in the right half
                     left = mid + 1
                 else:
                     right = mid - 1
@@ -28,3 +24,4 @@ s = Solution()
 assert s.search([4,5,6,7,0,1,2], 0) == 4
 assert s.search([4,5,6,7,0,1,2], 3) == -1
 assert s.search([1], 0) == -1
+assert s.search([1,3], 3) == 1
