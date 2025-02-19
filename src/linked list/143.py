92ecd32221e5dee7b0123dfe00c8a528008f605f
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-02-19 22:58:45 +0100

    solved 143

---
+++ src/linked list/143.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next or not head.next.next:
+            return
+
+        fast, slow = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        prev, curr = None, slow.next
+        slow.next = None
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
+        
+        
+            
+        
