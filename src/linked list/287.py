fc9338951580bbe9238cbadbfc642f080952ff57
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-04-22 17:47:33 +0200

    resolved 287

--- src/linked list/287.py
+++ src/linked list/287.py
@@ -1,20 +1,17 @@
 class Solution:
     def findDuplicate(self, nums: list[int]) -> int:
-        # Step 1: Find the intersection point
         slow = nums[0]
         fast = nums[0]
         
-        # Move slow by 1 step and fast by 2 steps to find intersection
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
             if slow == fast:
-                break  # Intersection found
+                break
         
-        # Step 2: Find the entrance to the cycle (duplicate number)
-        slow = nums[0]  # Reset slow to start
+        slow = nums[0]
         while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
         
-        return slow  # The duplicate number
+        
