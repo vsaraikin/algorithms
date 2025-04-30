243fedeb308be7ff3fabe0eb0a50d63b19be70e3
Vladimir Saraikin <saraikin.v@phystech.edu>
2025-04-30 15:18:34 +0200

    solved 230

---
+++ src/tree/230.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from node import TreeNode
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.count = 0
+        self.result = None
+
+        def inorder(node: Optional[TreeNode]):
+            if not node or self.result is not None:
+                return
+            
+            inorder(node.left)
+            self.count += 1
+            if self.count == k:
+                self.result = node.val
+                return
+            inorder(node.right)
+
+        inorder(root)
+        return self.result
