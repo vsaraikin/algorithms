926ac14917b34c0f61ba8adb4422d1728f34b073
Vladimir Saraikin <saraikin.v@phystech.edu>
2026-04-26 17:31:37 +0300

    solved 1

--- src/arrays-and-hashing/1/solution.cpp
+++ src/arrays-and-hashing/1/solution.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
 #include <assert.h>
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        return res;
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int comp = target - nums[i];
+            if (seen.count(comp))
+            {
+                return {seen[comp], i};
+            }
+            seen[nums[i]] = i;
+        }
+        return {};
     }
 };
 
-int main() {
+int main()
+{
     Solution s = Solution();
-    vector<int> input1 = {2,7,11,15};
-    vector<int> output1 = {0,1};
-    assert(s.twoSum(input1, 9) == output1);
+
+    vector<int> input1 = {2, 7, 11, 15};
+    assert(s.twoSum(input1, 9) == (vector<int>{0, 1}));
+
+    vector<int> input2 = {3, 2, 4};
+    assert(s.twoSum(input2, 6) == (vector<int>{1, 2}));
+
+    vector<int> input3 = {3, 3};
+    assert(s.twoSum(input3, 6) == (vector<int>{0, 1}));
+
+    vector<int> input4 = {0, 4, 3, 0};
+    assert(s.twoSum(input4, 0) == (vector<int>{0, 3}));
+
+    vector<int> input5 = {-3, 4, 3, 90};
+    assert(s.twoSum(input5, 0) == (vector<int>{0, 2}));
+
     return 0;
 }
