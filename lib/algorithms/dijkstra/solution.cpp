f1e9cdbe5a5875ee49a31b02a97c93534af42ce8
Vladimir Saraikin <saraikin.v@phystech.edu>
2026-08-21 13:48:58 +0300

    update

--- lib/algorithms/dijkstra/solution.cpp
+++ lib/algorithms/dijkstra/solution.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <climits>
 #include <functional>
 #include <queue>
 #include <utility>
@@ -9,43 +10,43 @@ std::vector<int> dijkstra(int src, const std::vector<std::vector<std::pair<int, 
 	int n = adj.size();
 	std::vector<int> dist(n, INT_MAX);
 	dist[src] = 0;
-
 	using Item = std::pair<int, int>;
+
 	std::priority_queue<Item, std::vector<Item>, std::greater<Item>> pq;
 	pq.push({0, src});
-
 	while (!pq.empty()) {
-		auto [d, u] = pq.top();
+		auto [d, u] = pq.top(); // distance, current node
 		pq.pop();
-
-		if (d > dist[u]) {
+		if (dist[u] < d) {
 			continue;
 		}
-
-		for (auto [v, w]: adj[u]) {
-			if (dist[u] + w < dist[v]) {
-				dist[v] = dist[u] + w;
-				pq.emplace(dist[v], v);
+		
+		for (auto [nei_v, nei_w]: adj[u]) {
+			if (dist[nei_v] > nei_w +  d) {
+				pq.push({nei_w + d, nei_v});
+				dist[nei_v] = nei_w + d;
 			}
+			
 		}
+		
 	}
 	return dist;
 }
 
 int main() {
 	{
-		std::vector<std::vector<std::pair<int, int>>> adj(6);
-		adj[0] = {{1, 4}, {2, 1}};
-		adj[1] = {{3, 1}};
-		adj[2] = {{1, 2}, {3, 6}, {4, 7}};
-		adj[3] = {{4, 3}};
+		std::vector<std::vector<std::pair<int, int>>> adj(7);
+		adj[1] = {{2, 4}, {3, 1}};
+		adj[2] = {{4, 1}};
+		adj[3] = {{2, 2}, {4, 6}, {5, 7}};
+		adj[4] = {{5, 3}};
 
-		auto dist = dijkstra(0, adj);
-		assert(dist[0] == 0);
-		assert(dist[1] == 3);
-		assert(dist[2] == 1);
-		assert(dist[3] == 4);
-		assert(dist[4] == 7);
-		assert(dist[5] == INT_MAX);
+		auto dist = dijkstra(1, adj);
+		assert(dist[1] == 0);
+		assert(dist[2] == 3);        // 1 -> 3 -> 2
+		assert(dist[3] == 1);
+		assert(dist[4] == 4);        // 1 -> 3 -> 2 -> 4
+		assert(dist[5] == 7);        // 1 -> 3 -> 2 -> 4 -> 5
+		assert(dist[6] == INT_MAX);  // isloated
 	}
 }
